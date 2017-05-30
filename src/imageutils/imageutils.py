032b7dbd4986c0fef2fbb75066d27c90354da6e0
skoos <skoos@heuritech.com>
2017-05-30 11:49:28 +0200

    Small modifs

--- src/imageutils/imageutils.py
+++ src/imageutils/imageutils.py
@@ -1,8 +1,10 @@
 """ Library for image processing """
 import os
+import logging
 from PIL import Image
 import numpy as np
 
+LOGGER = logging.getLogger('imageutils')
 
 def img_to_array(img, data_format='channels_last', data_type=np.float32):
     """Converts a PIL Image instance to a Numpy array.
@@ -15,7 +17,9 @@ def img_to_array(img, data_format='channels_last', data_type=np.float32):
       ValueError: if invalid `img` or `data_format` is passed.
     """
     if data_format not in {'channels_first', 'channels_last'}:
-        raise ValueError('Unknown data_format: ', data_format)
+        LOGGER.WARNING('Unknown/missing data format in img_to_array function: ' + data_format)
+        return None
+        #raise ValueError('Unknown data_format: ', data_format)
     # Numpy array x has format (height, width, channel)
     # or (channel, height, width)
     # but original PIL image has format (width, height, channel)
@@ -30,21 +34,32 @@ def img_to_array(img, data_format='channels_last', data_type=np.float32):
         else:
             img_data = img_data.reshape((img_data.shape[0], img_data.shape[1], 1))
     else:
-        raise ValueError('Unsupported image shape: ', img_data.shape)
+        LOGGER.WARNING('Unsupported image shape in img_to_array function: ' + str(img_data.shape))
+        return None
+        #raise ValueError('Unsupported image shape: ', img_data.shape)
     return img_data
 
 
-def resize_img(img, target_size=None):
+def resize_img(img, target_size):
     """Return resized PIL image"""
+    if target_size is None \
+       or not isinstance(target_size, (list, tuple, np.ndarray)) \
+       or len(target_size) != 2:
+        LOGGER.WARNING('Unexpected size dimension for resize_img function: ' + str(target_size))
+        return None
     hw_tuple = (target_size[1], target_size[0])
     if img.size != hw_tuple:
         img = img.resize(hw_tuple, Image.ANTIALIAS)
     return img
 
 
-def crop_img(img, target_crop=None):
+def crop_img(img, target_crop):
     """Returns cropped PIL image"""
-    assert len(target_crop) == 4
+    if target_crop is None \
+       or not isinstance(target_crop, (list, tuple, np.ndarray)) \
+       or len(target_crop) != 4:
+        LOGGER.WARNING('Unexpected crop dimension for crop_img function: ' + str(target_crop))
+        return None
     img = img.crop(target_crop)
     return img
 
@@ -62,10 +77,14 @@ def load_img(path, target_size=None):
         ImportError: if PIL is not available.
     """
     if Image is None:
-        raise ImportError('Could not import PIL.Image. '
-                          'The use of `load_img` requires PIL.')
+        LOGGER.WARNING('Could not import PIL.Image. The use of `load_img` requires PIL.')
+        return None
+        #raise ImportError('Could not import PIL.Image. '
+        #                  'The use of `load_img` requires PIL.')
     if not os.path.exists(path):
-        raise ValueError('Directory not found: ', path)
+        LOGGER.WARNING('Directory not found ing load_img function: ' + path)
+        return None
+        #raise ValueError('Directory not found: ', path)
     img = Image.open(path)
     if img.mode != 'RGB':
         img = img.convert('RGB')
@@ -74,9 +93,10 @@ def load_img(path, target_size=None):
     return img
 
 
-def save_img(img, output_path=None):
+def save_img(img, output_path, quality=95):
     """Saves PIL image with a provided filename"""
     if not output_path or not os.path.exists(os.path.dirname(output_path)):
-        raise ValueError('Directory not found: ', os.path.dirname(output_path))
+        LOGGER.WARNING('Directory not found in save_img function: ' + output_path)
+        #raise ValueError('Directory not found: ', os.path.dirname(output_path))
     else:
-        img.save(output_path, 'jpeg', quality=95)
+        img.save(output_path, 'jpeg', quality=quality)
