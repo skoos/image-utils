0527b7e96735936bf5192369f6ae7d6feccd4f7f
skoos <skoos@heuritech.com>
2017-05-30 13:36:44 +0200

    Handling md5 (for loading/computing)

--- src/imageutils/imageutils.py
+++ src/imageutils/imageutils.py
@@ -12,7 +12,7 @@ LOGGER = logging.getLogger('imageutils')
 def img_to_array(img, data_format='channels_last', data_type=np.float32):
     """Converts a PIL Image instance to a Numpy array."""
     if data_format not in {'channels_first', 'channels_last'}:
-        LOGGER.WARN('Unknown/missing data format in img_to_array function: ' + data_format)
+        LOGGER.warning('Unknown/missing data format in img_to_array function: ' + data_format)
         return None
         #raise ValueError('Unknown data_format: ', data_format)
     # Numpy array x has format (height, width, channel)
@@ -29,7 +29,7 @@ def img_to_array(img, data_format='channels_last', data_type=np.float32):
         else:
             img_data = img_data.reshape((img_data.shape[0], img_data.shape[1], 1))
     else:
-        LOGGER.WARN('Unsupported image shape in img_to_array function: ' + str(img_data.shape))
+        LOGGER.warning('Unsupported image shape in img_to_array function: ' + str(img_data.shape))
         return None
         #raise ValueError('Unsupported image shape: ', img_data.shape)
     return img_data
@@ -40,7 +40,7 @@ def resize_img(img, target_size):
     if target_size is None \
        or not isinstance(target_size, (list, tuple, np.ndarray)) \
        or len(target_size) != 2:
-        LOGGER.WARN('Unexpected size dimension for resize_img function: ' + str(target_size))
+        LOGGER.warning('Unexpected size dimension for resize_img function: ' + str(target_size))
         return None
     hw_tuple = (target_size[1], target_size[0])
     if img.size != hw_tuple:
@@ -53,7 +53,7 @@ def crop_img(img, target_crop):
     if target_crop is None \
        or not isinstance(target_crop, (list, tuple, np.ndarray)) \
        or len(target_crop) != 4:
-        LOGGER.WARN('Unexpected crop dimension for crop_img function: ' + str(target_crop))
+        LOGGER.warning('Unexpected crop dimension for crop_img function: ' + str(target_crop))
         return None
     img = img.crop(target_crop)
     return img
@@ -62,12 +62,12 @@ def crop_img(img, target_crop):
 def load_img(path, target_size=None):
     """Loads an image into PIL format."""
     if Image is None:
-        LOGGER.WARN('Could not import PIL.Image. The use of `load_img` requires PIL.')
+        LOGGER.warning('Could not import PIL.Image. The use of `load_img` requires PIL.')
         return None
         #raise ImportError('Could not import PIL.Image. '
         #                  'The use of `load_img` requires PIL.')
     if not os.path.exists(path):
-        LOGGER.WARN('Directory not found ing load_img function: ' + path)
+        LOGGER.warning('Directory not found ing load_img function: ' + path)
         return None
         #raise ValueError('Directory not found: ', path)
     img = Image.open(path)
@@ -81,7 +81,7 @@ def load_img(path, target_size=None):
 def save_img(img, output_path, quality=95):
     """Saves PIL image with a provided filename."""
     if not output_path or not os.path.exists(os.path.dirname(output_path)):
-        LOGGER.WARN('Directory not found in save_img function: ' + output_path)
+        LOGGER.warning('Directory not found in save_img function: ' + output_path)
         #raise ValueError('Directory not found: ', os.path.dirname(output_path))
     else:
         img.save(output_path, 'jpeg', quality=quality)
@@ -94,7 +94,7 @@ def load_img_from_md5(image_md5, url='https://octopus.heuritech.com/get?md5='):
         file_data = io.BytesIO(urllib.request.urlopen(md5_url).read())
         img = Image.open(file_data)
     except ValueError as error:
-        LOGGER.WARN('Error in load_img_from_md5: ' + str(error))
+        LOGGER.warning('Error in load_img_from_md5: ' + str(error))
         return None
     return img
 
@@ -102,7 +102,7 @@ def load_img_from_md5(image_md5, url='https://octopus.heuritech.com/get?md5='):
 def get_md5_from_imagepath(image_path):
     """Computes and returns md5 from image (providing its path)."""
     if image_path is None or not os.path.exists(os.path.dirname(image_path)):
-        LOGGER.WARN('Directory not found in get_md5_from_imagepath function: ' + image_path)
+        LOGGER.warning('Directory not found in get_md5_from_imagepath function: ' + image_path)
         return None
     hash_md5 = hashlib.md5()
     with open(image_path, "rb") as file_data:
